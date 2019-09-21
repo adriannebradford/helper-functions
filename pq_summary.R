fe124a95d2fb249775c7e5bbb1d61b1b229fa893
adriannebradford <adriannebradford@gmail.com>
2019-09-21 18:29:30 -0400

    remove pq_chisq until i finish build_csq

--- pq_summary.R
+++ pq_summary.R
@@ -51,35 +51,11 @@ pq_summary <- function(tname = "Summary Statistics", varlist){
     return(x)
 }
 
-pq_chisq <- function(varlist, tname = "Chi-Square Results"){
-  newdf <- build_csq(varlist)
-  varhead <- c("x" = 2, names(varlist[2]) = 5)
-  names(varhead) <- c(" ", names(varlist[2]))
-  tab_width <- length(newdf)
-  titlehead <- c(tname = tab_width)
-  names(titlehead) <- tname
-  al <- c("c", "l", rep("c", length(newdf) - 2))
-  out <- newdf2 %>% 
-    kable(align = al, booktabs=T) %>% 
-    kable_styling(full_width = FALSE, bootstrap_options = "condensed") %>% 
-    row_spec(0, extra_css = "border-bottom: solid thin;") %>%
-    column_spec(2, extra_css = "font-size: xx-small;") %>% 
-    add_header_above(header = varhead, 
-                     align = "c", extra_css = "border-top: solid thin; border-bottom: solid thin;") %>% 
-    add_header_above(header = titlehead, 
-                     align = "l", extra_css = "border-top: solid; border-bottom: double;") %>% 
-    row_spec(which(newdf[2] == "ChiSq"), 
-             extra_css = "border-bottom: solid thin; border-top: initial; vertical-align: middle; line-height: 6px;") %>%
-    row_spec(which(newdf[2] == "Frequency" | newdf[2] == "Percent"), 
-             extra_css = "border-top: initial; vertical-align: middle; line-height: 6px;") %>%
-    column_spec(1, bold=TRUE, extra_css = "border-bottom: solid thin;") %>%         
-    collapse_rows(columns = 1, valign = "middle")  %>% 
-    row_spec(nrow(newdf2), bold = T, extra_css = "border-bottom: solid; border-top: double; font-size: small;") 
-  return(out)
-}
-
 tb_spread <- function(x, xstat){
   x <- x %>% spread(x2, xstat)
   x <- x %>% mutate(stat = xstat) %>% select(1, last_col(), 2:length(x))
   return(x)
 }
+
+
+
